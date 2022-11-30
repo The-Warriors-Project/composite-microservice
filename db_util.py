40834c8a4903cdb285078626a594ea0e4fa5b34d
Yinon-Shirazi <ys3440@columbia.edu>
2022-11-30 10:58:58 -0500

    Added user deletion functionality

--- db_util.py
+++ db_util.py
@@ -1,4 +1,5 @@
 import requests
+import endpoints
 
 
 def delete_user_by_id(user_id: int):
@@ -6,26 +7,97 @@ def delete_user_by_id(user_id: int):
     param: user_id: id for query
     return: json with success/ Failure
     """
-    users_endpoint = "http://52.87.245.235:5011/api/v1/users/delete/user_id/"
-    reviews_endpoint = "http://44.211.188.169:5011/api/v1/reviews/delete/user_id/"
-    users_endpoint += str(user_id)
-    reviews_endpoint += str(user_id)
 
-    res_users = requests.delete(users_endpoint)
-    if not res_users:
-        return {"success": False, "payload": "No response from users MC"}
+    deletion = True
+    likes_offset = -1
 
-    # TODO: add functionality for books MC (if we want).
+    user_books = get_user_books(user_id)
+    if not user_books or not user_books["success"]:
+        return {"success": False, "payload": "users MC failed fetching books"}
 
-    res_reviews = requests.delete(reviews_endpoint)
-    if not res_users:
-        return {"success": False, "payload": "No response from reviews MC"}
+    res_users = update_users(user_id,deletion)
+    if not res_users or not res_users["success"]:
+        return {"success": False, "payload": "users MC failed deleting user"}
+
+    book_ids = user_books["payload"]
+    res_books = update_books(book_ids,likes_offset)
+
+    if not res_books or not res_books["success"]:
+        #reverting user's soft deletion
+        deletion = False
+        revert_users = update_users(user_id, deletion)
+        if not revert_users or not revert_users["success"]:
+            return {"success": False, "payload": "books MC failed updating and users failed to revert"}
+
+        return {"success": False, "payload": "books MC failed updating"}
+
+    res_reviews = update_reviews(user_id)
+    if not res_reviews or not res_reviews["success"]:
+        error_msg = "Reviews MC failed to update"
+        failed = []
+
+        # reverting user's soft deletion
+        deletion = False
+        revert_users = update_users(user_id, deletion)
+        if not revert_users or not revert_users["success"]:
+            failed.append("Users")
+
+        # reverting Books decrements
+        likes_offset = +1
+        revert_books = update_books(book_ids, likes_offset)
+        if not revert_books or not revert_books["success"]:
+            failed.append("Books")
+
+        if failed:
+            error_msg += " and " + ' & '.join(failed) + " failed to revert."
+
+        return {"success": False, "payload": error_msg}
 
     return {
         "success": True,
         "payload": {
-            "users_payload": res_users,
-            "reviews_payload": res_reviews
+            "user_id": user_id
         }
     }
+
+def update_users(user_id: int, deletion: bool):
+    """
+        param: user_id: id for query
+        return: json with from users MC
+    """
+    users_endpoint = endpoints.USERS + str(user_id)
+    payload = {"disable" : deletion}
+    return requests.put(users_endpoint, data= payload)
+
+def get_user_books(user_id:int):
+    """
+        param: user_id: id for query
+        return: json with success and book_ids or Failure
+    """
+
+    users_endpoint = "http://52.87.245.235:5011/api/v1/users/book_shelf/"
+    users_endpoint += str(user_id)
+    return requests.get(users_endpoint)
+
+def update_books(book_ids: list[int], likes_offset: int):
+    """
+        param: user_id: book_ids to be updated.
+        param: likes_offset: increment/decrement likes.
+        return: json with success and book_ids or Failure
+    """
+    books_endpoint = endpoints.BOOKS
+    payload = {
+        "offset": likes_offset,
+        "book_ids": book_ids
+               }
+    return requests.put(books_endpoint, data=payload)
+
+def update_reviews(user_id: int, deletion = True):
+    """
+        param: user_id: id for soft deletion
+        return: json with success and user_id or Failure
+    """
+    reviews_endpoint = endpoints.REVIEWS + str(user_id)
+    payload = {"disable": deletion}
+    return requests.put(reviews_endpoint, data=payload)
 
--- pull_request_template.md
+++ pull_request_template.md
@@ -1,4 +1,9 @@
 ### Description
 
 - [ ] Small Update
-- [ ] Big Update 
+- [X] Big Update
+- Added DELETE endpoint to be called from the frontend.
+- Added PUT call for users MC - soft deletion.
+- Added PUT call for books MC - adjust likes.
+- Added PUT call for reviews MC - soft deletion.
+- Implemented revert logic for failures. 
