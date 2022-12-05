8f03be6cf40ab249298d42c2e9067d3e738aa654
Yinon-Shirazi <ys3440@columbia.edu>
2022-12-04 19:02:49 -0500

    changed path params to user_name

--- db_util.py
+++ db_util.py
@@ -2,20 +2,20 @@ import requests
 import endpoints
 
 
-def delete_user_by_id(user_id: int):
+def delete_user_by_user_name(user_name: str):
     """
-    param: user_id: id for query
+    param: user_name: id for query
     return: json with success/ Failure
     """
 
     deletion = True
     likes_offset = -1
 
-    user_books = get_user_books(user_id)
+    user_books = get_user_books(user_name)
     if not user_books or not user_books["success"]:
         return {"success": False, "payload": "users MC failed fetching books"}
 
-    res_users = update_users(user_id,deletion)
+    res_users = update_users(user_name,deletion)
     if not res_users or not res_users["success"]:
         return {"success": False, "payload": "users MC failed deleting user"}
 
@@ -25,20 +25,20 @@ def delete_user_by_id(user_id: int):
     if not res_books or not res_books["success"]:
         #reverting user's soft deletion
         deletion = False
-        revert_users = update_users(user_id, deletion)
+        revert_users = update_users(user_name, deletion)
         if not revert_users or not revert_users["success"]:
             return {"success": False, "payload": "books MC failed updating and users failed to revert"}
 
         return {"success": False, "payload": "books MC failed updating"}
 
-    res_reviews = update_reviews(user_id)
+    res_reviews = update_reviews(user_name)
     if not res_reviews or not res_reviews["success"]:
         error_msg = "Reviews MC failed to update"
         failed = []
 
         # reverting user's soft deletion
         deletion = False
-        revert_users = update_users(user_id, deletion)
+        revert_users = update_users(user_name, deletion)
         if not revert_users or not revert_users["success"]:
             failed.append("Users")
 
@@ -56,59 +56,59 @@ def delete_user_by_id(user_id: int):
     return {
         "success": True,
         "payload": {
-            "user_id": user_id
+            "user_name": user_name
         }
     }
 
-def update_users(user_id: int, deletion: bool):
+def update_users(user_name: int, deletion: bool):
     """
-        param: user_id: id for query
+        param: user_name: id for query
         return: json with from users MC
     """
-    users_endpoint = endpoints.USERS + '{' + str(user_id) + '}'
+    users_endpoint = endpoints.USERS + 'user_name/delete_user/{' + str(user_name) + '}'
     payload = {"disable" : deletion}
     return requests.put(users_endpoint, data= payload)
 
-def get_user_books(user_id:int):
+def get_user_books(user_name:int):
     """
-        param: user_id: id for query
+        param: user_name: id for query
         return: json with success and book_ids or Failure
     """
 
-    users_endpoint = endpoints.USERS + "books/{" + str(user_id) + '}'
+    users_endpoint = endpoints.USERS + "books/{" + str(user_name) + '}'
     return requests.get(users_endpoint)
 
 def update_books(book_ids: list[int], likes_offset: int):
     """
-        param: user_id: book_ids to be updated.
+        param: user_name: book_ids to be updated.
         param: likes_offset: increment/decrement likes.
         return: json with success and book_ids or Failure
     """
-    books_endpoint = endpoints.BOOKS
+    books_endpoint = endpoints.BOOKS + "likes_count"
     payload = {
         "offset": likes_offset,
         "book_ids": book_ids
                }
     return requests.put(books_endpoint, data=payload)
 
-def update_reviews(user_id: int, deletion = True):
+def update_reviews(user_name: int, deletion = True):
     """
-        param: user_id: id for soft deletion
-        return: json with success and user_id or Failure
+        param: user_name: id for soft deletion
+        return: json with success and user_name or Failure
     """
-    reviews_endpoint = endpoints.REVIEWS + '{' + str(user_id) + '}'
+    reviews_endpoint = endpoints.REVIEWS + '{' + str(user_name) + '}'
     payload = {"disable": deletion}
     return requests.put(reviews_endpoint, data=payload)
 
 
 
-def get_book_shelf(user_id: int):
+def get_book_shelf(user_name: int):
     """
-    param: user_id: user from which to obtain book_ids
+    param: user_name: user from which to obtain book_ids
     return: json with all books info or failure
     """
 
-    user_books = get_user_books(user_id)
+    user_books = get_user_books(user_name)
     if not user_books or not user_books["success"]:
         return {"success": False, "payload": "users MC failed fetching books"}
 
